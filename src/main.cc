8641378f90f4be3f6f4bd8c64a4227db12923156
Travis <travis_chamness1@my.vcccd.edu>
2018-09-29 19:58:13 -0700

    Its GOOD

---
+++ src/main.cc
@@ -0,0 +1,25 @@
+/**
+* CS V15 Data Structures and Algorithms
+* CRN:
+*Assignment : ex03 - Fibonacci Sequence
+*
+* Statement of code ownership : I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @author travis_chamness1@my.vcccd.edu
+*/
+
+#include<iostream>
+#include "fibonacci.h"
+
+using std::cout;
+using std::endl;
+using edu::vcccd::vc::csv15::fibonacci;
+
+int main() {
+
+
+    cout << fibonacci(10) << endl;
+
+    system("PAUSE");
+}
